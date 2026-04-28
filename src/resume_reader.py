12b19fc256c313ad560cb86427b4f0860baecc06
SyamPrasadLaveti <syamprasad69@gmail.com>
2026-04-28 10:43:14 +0530

    Phase-3 Completed

--- src/resume_reader.py
+++ src/resume_reader.py
@@ -1,5 +1,6 @@
 import pdfplumber
-
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 def extract_text_from_pdf(file_path):
     text = ""
     with pdfplumber.open(file_path) as pdf:
@@ -8,39 +9,28 @@ def extract_text_from_pdf(file_path):
                 text += page.extract_text().lower() + "\n"
     return text
 
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
-skills = ["python", "sap abap", "java", "docker"]
 job_desc = """We are seeking a motivated Associate Software Engineer to join our development team.
-In this role, you will work alongside senior engineers to design, build, and maintain scalable software applications.
+In this role, you will work alongside senior engineers to design, build, and maintain scalable software applications using Sap abap,python and c++.
 This is an ideal position for early-career professionals looking to apply their multi-language skills
 (Python, Java, C) in a dynamic, containerized environment.""".lower()
 
+resume_text = extract_text_from_pdf("data/sample_resume.pdf")
+resume_embedding = model.encode(resume_text)
+jd_embedding = model.encode(job_desc)
 
-def score_of_resume(skills, resume_text, job_desc):
-    matched = []
-    missing = []
-    extra = []
+similarity_score=cosine_similarity([resume_embedding],[jd_embedding])[0][0]
 
-    for skill in skills:
-        if skill in job_desc and skill in resume_text:
-            matched.append(skill)
-        elif skill in job_desc:
-            missing.append(skill)
-        elif skill in resume_text:
-            extra.append(skill)
+if similarity_score >= 0.7:
+    decision = "Strong Match"
+elif similarity_score >=0.5:
+    decision = "Moderate Match"
+else:
+    decision = "Weak Match"
 
-    score = (len(matched) / len(skills)) * 100
-
-    return score, matched, missing, extra
-
-
-resume_text = extract_text_from_pdf("../data/sample_resume.pdf")
-
-score, matched_skills, missing_skills, extra_skills = score_of_resume(skills, resume_text, job_desc)
-    
 
 print("The Resume Score")
-print(f"Score: {score:.2f}%")
-print("Matched Skills:", matched_skills)
-print("Missing Skills:", missing_skills)
-print("Extra Skills:", extra_skills)
+print(f"Similarity score: {similarity_score:.4f}")
+print("Decision",decision)
+
